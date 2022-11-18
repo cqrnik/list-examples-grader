121ead45ed162af24c486ec32f288ef3f0b0f150
Nikhil <ndeepak@ucsd.edu>
2022-11-18 13:46:19 -0800

    lab 7

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,76 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class TestListExamples {
-  // Write your grading tests here!
+
+  @Test
+  public void testMerge() {
+    List<String> input1 = new ArrayList<String>();
+    List<String> input2 = new ArrayList<String>();
+
+    input1.add("apple");
+    input1.add("child");
+    input1.add("ears");
+    input1.add("grape");
+
+    input2.add("bear");
+    input2.add("energy");
+    input2.add("fan");
+    input2.add("zebra");
+
+    List<String> output1 = ListExamples.merge(input1, input2);
+    List<String> output2 = new ArrayList<String>();
+
+    output2.add("apple");
+    output2.add("bear");
+    output2.add("child");
+    output2.add("ears");
+    output2.add("energy");
+    output2.add("fan");
+    output2.add("grape");
+    output2.add("zebra");
+
+    assertEquals(8, output1.size());
+    assertEquals(output1, output2);
+
+  }
+
+  @Test
+  public void testMergeOneString() {
+    List<String> input1 = new ArrayList<String>();
+    List<String> input2 = new ArrayList<String>();
+
+    input1.add("apple");
+
+    List<String> output1 = ListExamples.merge(input1, input2);
+    List<String> output2 = new ArrayList<String>();
+
+    output2.add("apple");
+
+    assertEquals(1, output1.size());
+    assertEquals(output1, output2);
+
+  }
+
+  @Test
+  public void testMergeOne() {
+    List<String> input1 = new ArrayList<String>();
+    List<String> input2 = new ArrayList<String>();
+
+    input2.add("apple");
+
+    List<String> output1 = ListExamples.merge(input1, input2);
+    List<String> output2 = new ArrayList<String>();
+
+    output2.add("apple");
+
+    assertEquals(1, output1.size());
+    assertEquals(output1, output2);
+
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,46 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+
+cd student-submission
+
+
+
+
+if [ -f ListExamples.java ]
+then 
+    echo "file exists in given repository"
+else 
+    echo "file does not exist in given repository"
+    exit
+fi
+
+
+cp ../TestListExamples.java ./
+
+set +e
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" TestListExamples.java 2> ListCompile.txt
+if [ $? -eq 0 ]
+then 
+    echo "file compiles correctly"
+    echo "file compiles correctly" > ListCompile.txt
+    
+else 
+    echo "file does not compile correctly"
+    exit
+fi
+set -e
+echo "running tests..."
+java -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > ListTestResult.txt
+if [ $? -ne 0 ]
+then 
+    java -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples 2> ListTestResult.txt
+fi
+    
+echo "GRADING:"
+
+echo []
+
+
+
+
